d03651df2575b255631a9a0435686da1d033f58d
iMohamedMamdouh <imohamedmamdoh1@gmail.com>
2025-04-21 17:51:40 +0200

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ChatBot());
+}
+
+class ChatBot extends StatelessWidget {
+  const ChatBot({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
